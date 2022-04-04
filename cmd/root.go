8092c03b4dfdb1ac4704d81f84e658937794b03b
James Clarke <james@clarkezone.net>
2022-04-04 05:36:25 -0700

    Add root command

---
+++ cmd/root.go
@@ -0,0 +1,75 @@
+/*
+Copyright © 2022 Clarkezone
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "manimule",
+	Short: "A tool for working with kubernetes and docker manifests",
+	Long: `Manimule makes it easy to add and refactor docker and kubernetes manifests to an applicaiton. For example:
+
+manimule docker add
+manimule k8s clean deployment.yaml
+manimule k8s add kustomize
+manimule k8s layer create
+`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.manimule.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".manmule" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".manmule")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
--- main.go
+++ main.go
@@ -1,43 +1,7 @@
 package main
 
-import (
-	"log"
-	"os"
-	"unicode"
-
-	refactorCMD "github.com/clarkezone/rk/cmd/refactor"
-)
+import "github.com/clarkezone/rk/cmd"
 
 func main() {
-	err := refactorCMD.MakeOverlay().Execute()
-
-	//	root := &cobra.Command{
-	//		Use:           fmt.Sprintf("%s COMMAND [ARG...]", "rk"),
-	//		Short:         "Refactoring for kubernetes kustomize manifests",
-	//		Long:          "Refactoring for kubernetes kustomize manifests",
-	//		SilenceErrors: true,
-	//		PersistentPreRun: func(ccmd *cobra.Command, args []string) {
-	//			ccmd.SilenceUsage = false
-	//
-	//		},
-	//		PersistentPostRun: func(ccmd *cobra.Command, args []string) {
-	//			log.Printf("done")
-	//		},
-	//	}
-	//
-	//	root.AddCommand(refactorCMD.Refactor())
-	//	root.AddCommand(refactorCMD.MakeOverlay())
-	//
-	//	err := root.Execute()
-	//
-	if err != nil {
-		message := err.Error()
-		if len(message) > 0 {
-			tmp := []rune(message)
-			tmp[0] = unicode.ToUpper(tmp[0])
-			message = string(tmp)
-		}
-		log.Fatal(message)
-		os.Exit(1)
-	}
+	cmd.Execute()
 }
