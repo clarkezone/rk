4c8e7674d579100b4327462519b3efce14fb922a
James Clarke <james@clarkezone.net>
2022-04-08 01:06:11 -0700

    Add overlay command parsing (#11)
    * Overlay
    
    * Basic wireup for make overlay

---
+++ cmd/overlay.go
@@ -0,0 +1,55 @@
+package cmd
+
+import (
+	"fmt"
+
+	refactorCMD "github.com/clarkezone/rk/pkg/cmd/refactor"
+	"github.com/spf13/cobra"
+)
+
+//Layer
+func Overlay() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "overlay transform, add",
+		Short: "restructure source as base and overlays.",
+		Args:  NoArgsAccepted(),
+	}
+	cmd.AddCommand(Create())
+	return cmd
+}
+
+//Create creates a layer
+func Create() *cobra.Command {
+	var outdir string
+	var namespace string
+	command := &cobra.Command{
+		Use:   "create",
+		Short: "restructure source as base and overlays.",
+		Long: `rk overlay create restructures an existing flat set of kubernetes manifests and creates a base with a set of overlays.
+	By default this is, dev, stage, prod
+	Examples:
+		rk overlay create .
+		rk overlay create . --out \tmp\thing
+		`,
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return refactorCMD.DoMakeOverlay(
+				args[0],
+				[]string{"dev", "staging", "prod"},
+				outdir,
+				namespace)
+		},
+	}
+	command.LocalFlags().StringVar(&outdir, "out", "output", "Specify an output directory")
+	command.LocalFlags().StringVar(&namespace, "namespace", "default", "Specify kubernetes namespace")
+	return command
+}
+
+func NoArgsAccepted() cobra.PositionalArgs {
+	return func(cmd *cobra.Command, args []string) error {
+		if len(args) > 0 {
+			return fmt.Errorf("no args")
+		}
+		return nil
+	}
+}
